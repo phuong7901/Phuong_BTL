6ef5be301511cf6727ded63ed611bf7cd28fcec5
TrungKien07102001 <90234857+TrungKien07102001@users.noreply.github.com>
2021-10-31 21:27:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTL</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    <div>
+        <div class="container">
+        <header>
+            <a href="#"><img src="img/Logo.png" alt=""></a>
+           
+            <nav>
+        <div class="menu">
+            
+            <ui>
+                <li><a href="#"> Đăng Ký </a></li>
+                <li><a href="#"> Đăng Nhập </a></li>
+            </ui>
+
+            </div>
+
+        </nav>
+
+        </header>
+        
+        <div id="anh4"><a href="#"><img src="img/anh4.png" alt=""></a></div>
+        
+
+        <div class="Email">
+            <h1>Trello giúp các nhóm đẩy nhanh công việc.</h1>
+            <p>Cộng tác, quản lý dự án và đạt đến đỉnh cao năng suất mới.
+               Từ tòa nhà cao tầng đến văn phòng tại nhà,
+               cách thức làm việc của nhóm bạn là duy nhất.
+               Hãy hoàn thành tất cả với Trello.</p>
+        </div>
+       
+        <form action="/signup"><input type="email" name="email" aria-label="Email" placeholder="Email." data-testid="ui-email-signup-input"></form>
+        <div class="DKMP">
+            <button>Đăng ký - hoàn toàn miễn phí</button>
+        </div>
+        
+        <div id="anh2"><a href="#"><img src="img/anh2.png" alt=""></a></div>
+        <div class="Batdau">
+            <h2>Không chỉ là công việc.Đó là cách chúng ta làm việc cùng nhau.</h2>
+            <p>Bắt đầu với bảng Trello,danh sách và thẻ.
+                Tùy chỉnh và mở rộng với nhiều tính năng hơn khi hoạt động làm việc nhóm phát triển.
+                Quản lý dự án, sắp xếp tác vụ và tạo tinh thần làm việc nhóm—tất cả ở cùng một nơi.</p>
+        </div>
+        
+        <div class="DKMP">
+            <button>Bắt đầu công việc</button>
+        </div>
+
+        </div>
+        
+    </div>
+    <?php include('footer.php'); ?>
+
+</body>
+</html>
+</body>
+</html>
+
+
+
+
+
+
