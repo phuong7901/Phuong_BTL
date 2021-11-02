2d4b725624b3fb90124390033cbc3678eaf448ff
TrungKien07102001 <90234857+TrungKien07102001@users.noreply.github.com>
2021-11-02 17:30:39 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All rights reserved. Developed By - <a href="#">NTK</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
