416dac888c27c7181dbcc0c75fe032e70d91abac
TrungKien07102001 <90234857+TrungKien07102001@users.noreply.github.com>
2021-11-03 21:12:28 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,31 @@
+<?php 
+
+    include('../config/constants.php'); 
+    include('login-check.php');
+
+?>
+
+
+<html>
+    <head>
+        <title>Trello Website</title>
+
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/login.css">
+        <link rel="stylesheet" href="../css/register.css">
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class=" text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="../index.php">Nguyễn Trung Kiên</a></li>
+                    <li><a href="../login.php">Đăng Ký</a></li>
+                    <li><a href="../register.php">Đăng Nhập</a></li>
+                    <li><a href="../register.php">Đăng ký - hoàn toàn miễn phí</a></li>
+                    
+                </ul>
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
