dc272446b2fc6c22da83fadcd1b17e8b2903e7a9
phuong7901 <90234429+phuong7901@users.noreply.github.com>
2021-11-04 00:17:31 +0700

    login.form p2

--- login.php
+++ login.php
@@ -1,3 +1,47 @@
+<?php
+session_start();
+include("connect.php");
+
+if(isset($_POST["login"])){
+//
+  $email= $_POST["email"];
+  $password = md5($_POST["password"]);
+  //Nếu tồn tại click remember
+  if(isset($_POST["remember"])){
+    setcookie("email",$email);
+    setcookie("password",$_POST["password"]);
+  }
+
+//LỆNH SQL
+$sql_login = "SELECT * FROM users WHERE email='$email' AND password = '$password' ";
+
+  $result = mysqli_query($conn,$sql_login);
+  $row = mysqli_fetch_row($result);
+  if(count($row))
+  {
+    $_SESSION["login"] = $row;
+  }
+}
+
+//ád
+$email="";
+$password="";
+$check = false;
+if(isset($_COOKIE["email"]) && isset($_COOKIE["password"])){
+  $email=$_COOKIE["email"];
+  $password=$_COOKIE["password"];
+  $check = true;
+
+}
+if($email == 'email' && $password =='pass'){
+  $_SESSION["email"] = $email;
+  header('location:..');
+}else{
+  
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -17,26 +61,39 @@
           <div class="col-12 col-md-8 col-lg-6 col-xl-5">
             <div class="card bg-dark text-white" style="border-radius: 1rem;">
               <div class="card-body p-5 text-center">
-    
+                <form method="post">
                 <div class="mb-md-5 mt-md-4 pb-5">
     
                   <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                  <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                  <p class="text-white-50 mb-5">Please enter your email and password!</p>
     
                   <div class="form-outline form-white mb-4">
-                    <input type="email" id="typeEmailX" class="form-control form-control-lg" />
-                    <label class="form-label" for="typeEmailX">Email</label>
+                    <input type="email" id="email" class="form-control form-control-lg" 
+                    value = <?php 
+                                  echo $password
+                              ?>/>/>
+                    <label class="form-label" for="email">Email</label>
                   </div>
     
                   <div class="form-outline form-white mb-4">
-                    <input type="password" id="typePasswordX" class="form-control form-control-lg" />
-                    <label class="form-label" for="typePasswordX">Password</label>
+                    <input type="password" id="password" class="form-control form-control-lg" 
+                    value = <?php 
+                                  echo $password
+                              ?>/>
+                    <label class="form-label" for="password">Password</label>
                   </div>
+                  <div class="form-group">
+                  <label class = "custom-control custom-checkbox">
+                    <input <?php echo ($check)?"checked":""?>class="custom-control-input" type="checkbox" name="remember" value="1">
+                    <span class="custom-control-label">Remember Me</span>
+                  </label>
+                
+                </div>
     
-                  <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+                  <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="http://127.0.0.1:5500/btl2/register.html#!">Register Here</a></p>
     
-                  <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-    
+                  <button class="btn btn-outline-light btn-lg px-5" type="submit" name="login">Login</button>
+                </form>
                   <div class="d-flex justify-content-center text-center mt-4 pt-1">
                     <a href="#!" class="text-white"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
                     <a href="#!" class="text-white"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
